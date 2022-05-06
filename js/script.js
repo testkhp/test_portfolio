1c9a3bea028bf8efa06f55b26503ed8666fff4a5
testkhp <khp2337@naver.com>
2022-05-06 14:26:00 +0900

    포트폴리오 메인파일 및 css js 코드 수정

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 let test = document.querySelector(".test");
+let test2 = document.querySelector("span");
 
 test.addEventListener("click",function(){
 
     alert("경고창이 떠요!");
 
+});
+
+test2.addEventListener("mouseenter",function(){
+
+    alert("마우스 올렸네!");
 });
